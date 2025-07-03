8ef8fcd83905d29d9b2ff0cb624917f33c548568
Mona Lisa <ra1775@fayoum.edu.eg>
2025-07-03 13:25:40 +0300

    feat: add base conf and templates

--- rackflow/core/urls.py
+++ rackflow/core/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.shortcuts import render
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('',lambda req: render(req,'base.html')),
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
