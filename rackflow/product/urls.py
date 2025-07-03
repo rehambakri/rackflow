d3fcefc604e1a518f952f09e0849e303f5f8a505
Mazen Fahim <mazenfahim.g@gmail.com>
2025-07-03 17:36:19 +0300

    feat: add product app models

---
+++ rackflow/product/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+# This is used to establish a namespce.
+# Any path name that is defined here will be preappended by dashboard_
+# automatically by django if I want to referce this url it will be like thi
+# {% url dashboard:index %}
+app_name = "product"
+urlpatterns = [
+    path("", views.ProductList.as_view(), name="index"),
+]
