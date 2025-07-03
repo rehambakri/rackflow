5031b81b3046d4fbaafdfef09e69b4b182765519
Abdalhameed Ahmed <abdalhameed.med@gmail.com>
2025-07-03 11:43:06 +0300

    feat: add authentication app

---
+++ rackflow/authentication/forms.py
@@ -0,0 +1,23 @@
+# accounts/forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('email', 'first_name', 'last_name') # Add other fields as needed
+
+class CustomUserProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'profile_image'] # Fields the user can update
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Add Bootstrap classes or other custom styling
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.ClearableFileInput):
+                field.widget.attrs.update({'class': 'form-control-file'})
+            else:
+                field.widget.attrs.update({'class': 'form-control'})
--- rackflow/core/settings.py
+++ rackflow/core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "authentication.apps.AuthenticationConfig"
 ]
 
 MIDDLEWARE = [
@@ -121,7 +122,6 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'  # URL prefix for media files
 MEDIA_ROOT = BASE_DIR/'media'
-STATIC_ROOT=BASE_DIR/'static'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[
@@ -132,3 +132,4 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = 'authentication.CustomUser'
