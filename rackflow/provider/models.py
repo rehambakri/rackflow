ca7a953d03ffcdd12695f466ecf8e4e62b4d3869
Abdalhameed Ahmed <abdalhameed.med@gmail.com>
2025-07-04 18:31:25 +0300

    feat: add shipment, shipmentProduct models

--- rackflow/provider/models.py
+++ rackflow/provider/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from product.models import Product
 
 
 class Provider(models.Model):
@@ -10,3 +11,50 @@ class Provider(models.Model):
     class Meta:
         db_table = "providers"
         verbose_name_plural = "providers"
+
+
+class Shipment(models.Model):
+    STATUS_CHOICES = [
+        ("pending", "Pending"),
+        ("accepted", "Accepted"),
+        ("arrived", "Arrived"),
+    ]
+
+    provider = models.ForeignKey(
+        to=Provider, on_delete=models.RESTRICT, related_name="shipments"
+    )
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
+
+    c_date = models.DateTimeField(auto_now_add=True)
+    a_date = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
+    arr_date = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
+
+    products = models.ManyToManyField(
+        Product,
+        through="ShipmentProduct",
+        related_name="shipments",
+    )
+
+    def __str__(self):
+        return f"Shipment #{self.id} - Provider: {self.provider.name} - Status: {self.status}"
+
+
+class ShipmentProduct(models.Model):
+    shipment = models.ForeignKey(
+        Shipment, on_delete=models.CASCADE, related_name="shipment_products"
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        unique_together = ("shipment", "product")
+
+    def __str__(self):
+        return f"{self.product.name} ({self.quantity}) in Shipment #{self.shipment.id}"
