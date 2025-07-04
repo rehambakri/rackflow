41cf374968be68205753a1f0e19428471994991d
Abdalhameed Ahmed <abdalhameed.med@gmail.com>
2025-07-04 18:24:31 +0300

    feat: add provider model

--- rackflow/provider/models.py
+++ rackflow/provider/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Provider(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        db_table = "providers"
+        verbose_name_plural = "providers"
