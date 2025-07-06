04bd20f166ffd212385974a51930d39a76a2ba1d
reham <reehaam209@gmail.com>
2025-07-07 01:45:04 +0300

    feat: add pagination.

--- rackflow/product/views.py
+++ rackflow/product/views.py
@@ -14,12 +14,13 @@ class ProductList(ListView):
     model = Product
     template_name = "product/index.html"
     context_object_name = "products"
+    paginate_by= 10 
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         categories = Category.objects.all()
         context["categories"] = categories
-        return categories
+        return context
 
 
 class ProductCreate(CreateView):
