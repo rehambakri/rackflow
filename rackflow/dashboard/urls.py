53ffe380c72b5c74bd987c42d09461253d289c54
reham <reehaam209@gmail.com>
2025-07-10 18:21:04 +0300

    feat: create update user template

--- rackflow/dashboard/urls.py
+++ rackflow/dashboard/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
         path('add-employee/', views.Add_employee.as_view(), name='add_employee'),
-        path('list_critical_products.html',views.is_critical, name='is_critical'),
-]
+        path('list_critical_products',views.is_critical, name='is_critical'),
+         path('update-user-profile/<int:pk>/', views.UpdateEmployeeProfileView.as_view(), name='update_user_profile'), ]
 
 
 
