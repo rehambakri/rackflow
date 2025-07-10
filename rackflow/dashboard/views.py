47bfe988459fa625ced799f5bbab35fdcb74c0e9
Mona Lisa <ra1775@fayoum.edu.eg>
2025-07-10 19:26:56 +0300

    add list_users template

--- rackflow/dashboard/views.py
+++ rackflow/dashboard/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from product.models import Product
-
+from authentication.models import CustomUser
+from django.views.generic import ListView
 # Create your views here.
 
 
@@ -18,3 +19,15 @@ def is_critical(request):
     return render(
         request, "list_critical_products.html", {"iscritical": critical_products}
     )
+
+
+
+class ListUsersView(ListView):
+    model = CustomUser
+    template_name = "list_users.html"
+    context_object_name = "users"  
+
+    def get_queryset(self):
+        users = super().get_queryset()
+        print("Users in DB:", users)
+        return users
